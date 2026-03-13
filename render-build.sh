5e2bfec04273aef9b106c7ce80b24f882ab24686
AzDevcoder <adm.azdevcoder@gmail.com>
2026-03-13 16:28:03 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
+
+pip install --upgrade pip
 pip install -r requirements.txt
-playwright install chromium
+
+# Este comando tenta instalar as libs de sistema necessárias no Render
+playwright install --with-deps chromium
