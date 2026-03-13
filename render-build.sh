f2dd4a1097878de7d2746f00effe5999021421ff
AzDevcoder <adm.azdevcoder@gmail.com>
2026-03-13 16:18:27 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-# Sair se houver erro
 set -o errexit
-
-# Atualiza o pip e instala as dependências do Python
-pip install --upgrade pip
 pip install -r requirements.txt
-
-# Instala o binário do Chromium (SEM o comando --with-deps que pede root)
 playwright install chromium
